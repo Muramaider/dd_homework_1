aa4ade46bc8f0872ac007ffc0634b6e875c5d77b
Aleksey <aleksey@MacBook-Air-Aleksey.local>
2017-10-09 18:34:04 +0300

    Initial Commit

---
+++ calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by Aleksey on 09.10.17.
+//  Copyright © 2017 Aleksey Vinogradov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ calculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  calculator
+//
+//  Created by Aleksey on 09.10.17.
+//  Copyright © 2017 Aleksey Vinogradov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
